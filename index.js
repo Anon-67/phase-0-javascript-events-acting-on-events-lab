1297ca4fde54958a89315225b7fe7223e0d2cf49
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-07 15:59:01 -0600

    commit tree 5f7fa576171b4961ec1a4572f2d18e2f62dcd925

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers);
+      
+      if (left >0){
+        dodger.style.left = `${left - 10}px`;
+      }
+    } else if (event.key === "ArrowRight") {
+        const rightNumbers = dodger.style.left.replace("px", "");
+        const right = parseInt(rightNumbers);
+    
+        if (right < 360) {
+            dodger.style.left = `${right + 10}px`;
+        }
+      }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers);
+    
+    if (left >0){
+      dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 10}px`;
+    }
+}
